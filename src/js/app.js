988977dc02d61d5f7999afddca94d6f9192192db
Frunt <lncubus@7654@gmail.com>
2019-05-16 12:43:07 +0300

    js fix

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,6 @@
 /**
  * @module jQuery
  */
-import $ from 'jquery';
 
 /**
  * @module Popper.js
@@ -33,19 +32,3 @@ import '../vendors/mdb/js/addons/datatables';
 
 // Load our example file
 import './example';
-
-import slide2 from '../img/main-slider/slide-1.jpg';
-import slide1 from '../img/main-slider/desktop-slide-1.jpg';
-import logo from '../img/main-slider/mobile-slide-1.jpg';
-import eventsSlide1 from '../img/events-slider/slide-1.jpg';
-import eventsSlide2 from '../img/events-slider/slide-2.jpg';
-
-
-$('#main-slide-1').css('background-image', `url(${slide1})`);
-$('#main-slide-2').css('background-image', `url(${slide2})`);
-$('#events-1').css('background-image', `url(${eventsSlide1})`);
-$('#events-2').css('background-image', `url(${eventsSlide2})`);
-$('#events-3').css('background-image', `url(${eventsSlide1})`);
-$('#events-4').css('background-image', `url(${eventsSlide2})`);
-$('#events-5').css('background-image', `url(${eventsSlide1})`);
-$('#logo-header').src = logo;
